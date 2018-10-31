3eb0fd75989d81889bf8a08f131e23d548db03c6
Tardo <dev@redneboa.es>
2018-10-31 05:35:52 +0100

    [IMP] Travis

--- .travis_compile.sh
+++ .travis_compile.sh
@@ -4,12 +4,12 @@ cd $TRAVIS_BUILD_DIR
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	mkdir build-$CMAKE_BUILD_TYPE/ && cd build-$CMAKE_BUILD_TYPE/
-	cmake -DCMAKE_BUILD_TYPE:STRING=$CMAKE_BUILD_TYPE -DZPG_PACKER_BIN="$ZPG_PACKER_NAME" ..
+	cmake -DCMAKE_BUILD_TYPE:STRING=$CMAKE_BUILD_TYPE -DZPG_PACKER_BIN="$ZPG_PACKER_BIN" ..
 	make && make create_zpg
 	cpack --debug --verbose -C CPackConfig.cmake
 elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
 	mkdir build-$CMAKE_BUILD_TYPE/ && cd build-$CMAKE_BUILD_TYPE/
-    cmake -Werror=dev -G "$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DZLIB_ROOT="$ZLIB_ROOT" -DZPG_PACKER_BIN="$ZPG_ROOT\bin\$ZPG_PACKER_NAME.exe" ..
+    cmake -Werror=dev -G "$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DZLIB_ROOT="$ZLIB_ROOT" -DZPG_PACKER_BIN="$ZPG_PACKER_BIN" ..
     cd ..
     cmake --build build-$CMAKE_BUILD_TYPE --config $CMAKE_BUILD_TYPE --target MVRunner
     cmake --build build-$CMAKE_BUILD_TYPE --config $CMAKE_BUILD_TYPE --target create_zpg
