b6be2f549b0ea638112d62ab1a3f12c6bda220bb
Tardo <dev@redneboa.es>
2018-10-31 02:25:25 +0100

    [IMP] Travis

--- .travis_install_dependencies.sh
+++ .travis_install_dependencies.sh
@@ -14,20 +14,3 @@ wget -O ~/zpgv1.0.0.zip https://gitlab.com/Tardo/Zpg/-/archive/master/Zpg-master
 # Compile & Install SFML and ZPG
 cd ~/SFML-$SFML_VER; cmake -DBUILD_SHARED_LIBS:BOOL=FALSE -DCMAKE_INSTALL_PREFIX:PATH=/usr . && make && sudo make install
 cd ~/Zpg-master; cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr . && make Zpg && make zpg_packer && sudo make install
-
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-
-    # Install some custom requirements on OS X
-    # e.g. brew install pyenv-virtualenv
-
-    case "${TOXENV}" in
-        py32)
-            # Install some custom Python 3.2 requirements on OS X
-            ;;
-        py33)
-            # Install some custom Python 3.3 requirements on OS X
-            ;;
-    esac
-else
-    # Install some custom requirements on Linux
-fi
