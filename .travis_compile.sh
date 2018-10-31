957be58373f9f2b9c4c428582aa49f1043280084
Tardo <dev@redneboa.es>
2018-10-31 14:46:56 +0100

    [IMP] Travis

--- .travis_compile.sh
+++ .travis_compile.sh
@@ -8,10 +8,10 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
 	make && make create_zpg
 	cpack --debug --verbose -C CPackConfig.cmake
 elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
-	mkdir build-$CMAKE_BUILD_TYPE/ && cd build-$CMAKE_BUILD_TYPE/
+	mkdir build/ && cd build/
     cmake -Werror=dev -G "$CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$CMAKE_BUILD_TYPE -DZLIB_ROOT="$ZLIB_ROOT" -DZPG_PACKER_BIN="$ZPG_PACKER_BIN" ..
     cd ..
-    cmake --build build-$CMAKE_BUILD_TYPE --config $CMAKE_BUILD_TYPE --target MVRunner
-    cmake --build build-$CMAKE_BUILD_TYPE --config $CMAKE_BUILD_TYPE --target create_zpg
-    cmake --build build-$CMAKE_BUILD_TYPE --config $CMAKE_BUILD_TYPE --target package
+    cmake --build build --config $CMAKE_BUILD_TYPE --target MVRunner
+    cmake --build build --config $CMAKE_BUILD_TYPE --target create_zpg
+    cmake --build build --config $CMAKE_BUILD_TYPE --target package
 fi
